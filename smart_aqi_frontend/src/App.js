ec40476c11aa83670de6fc98d4cc89810cd9aba8
ruchitha183 <mallelaruchitha@gmail.com>
2025-12-29 22:20:34 +0530

    Update App.js

--- smart_aqi_frontend/src/App.js
+++ smart_aqi_frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import About from './pages/About'; // <-- Newly added
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} /> {/* NEW ROUTE */}
         </Routes>
       </Router>
     </ThemeProvider>
